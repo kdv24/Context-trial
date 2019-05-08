5fa1ddc4242e2c3455e3f919e5c0737ca23fbbd2
Kelly de Vries <kdv@me.com>
2019-05-08 12:38:01 -0700

    add hook for name/password update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserContext from '../UserContext';
+
+const Form = () => {
+    const [username, setUsername] = useState(null);
+    console.log('currentUser is: ', username);
+    return (
+        <form type="submit">
+            <label htmlFor="name">
+                Name:
+                <input
+                    className="input-fields"
+                    type="text"
+                    onChange={e => setUsername(e.target.value)}
+                />
+            </label>
+            <label htmlFor="password">
+                Password:
+                <input
+                    className="input-fields"
+                    type="text"
+                    onChange={e => setUsername(e.target.value)}
+                />
+            </label>
+        </form>
+    );
+};
 
 const Login = ({ onLogin }) => (
     <div className="login">
-        <p>Login here</p>
-        <form>
-            <label htmlFor="name">
-                Name:
-                <input type="text" />
-            </label>
-            <UserContext.Consumer>
-                {({ onLogin }) => <button onClick={onLogin}>Sign In</button>}
-            </UserContext.Consumer>
-        </form>
+        <p>Login to see your Messages and To Do list</p>
+        <Form />
+        <UserContext.Consumer>
+            {({ onLogin }) => (
+                <button
+                    onClick={onLogin}
+                    style={{
+                        backgroundColor: 'lightgray',
+                        borderRadius: '5px',
+                        width: '150px',
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        alignSelf: 'center',
+                        margin: '25px',
+                        padding: '12px',
+                    }}
+                >
+                    Sign In
+                </button>
+            )}
+        </UserContext.Consumer>
     </div>
 );
 
